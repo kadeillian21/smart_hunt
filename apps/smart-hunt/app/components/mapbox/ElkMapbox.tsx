47cedcf00ccce18fc1ab4cdd1c965db8d049f069
Kade Illian <kadeillianmt@gmail.com>
2023-02-11 02:32:45 -0700

    got the api hooked to the geojson layer

--- apps/smart-hunt/app/components/mapbox/ElkMapbox.tsx
+++ apps/smart-hunt/app/components/mapbox/ElkMapbox.tsx
@@ -16,16 +16,13 @@ const ElkAndDeerMap = () => {
   const [lng, setLng] = useState(-110.1);
   const [lat, setLat] = useState(47);
   const [zoom, setZoom] = useState(6.25);
-  const [elkData, setElkData] = useState<Elk[]>([]);
 
-  const fetchElkData = async () => {
-    try {
-      const response = await axios.get("http://localhost:4200/api/elk");
-      setElkData(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  async function fetchData() {
+    const response = await axios.get("http://localhost:4200/api/elk");
+    const elkData = response.data.data;
+    const filteredData = elkData.filter(elk => elk.license_year === 2021 && elk.residency === 'SUM');
+    return filteredData;
+  }
 
   useEffect(() => {
     const initializeMap = ({ setMap, mapContainer }) => {
@@ -92,15 +89,41 @@ const ElkAndDeerMap = () => {
           minzoom: 0,
           maxzoom: 22,
         });
-        mapInstance.on("click", "deer-and-elk-hunting-district-layer", (e) => {
-
+        mapInstance.on("click", "deer-and-elk-hunting-district-layer", async (e) => {
           const feature = e.features[0];
           const district = feature.properties.DISTRICT;
-          const districtElkData = elkData.filter((elk: Elk) => elk.hunting_district === district);
+          const response = await axios.get("http://localhost:4200/api/elk");
+          const elkData = (response.data);
+          const districtElkData = elkData.data.filter(elk => elk.hunting_district === district && elk.license_year === 2021 && elk.residency === 'SUM');
           let elkDataString = '';
           if (districtElkData.length > 0) {
-            districtElkData.forEach((elk: Elk) => {
-              elkDataString += `<p>Year: ${elk.license_year}</p><p>Population: ${elk.bow}</p>`;
+            districtElkData.forEach(elk => {
+              elkDataString +=
+              `<p>Year: ${elk.license_year}</p>
+              <p>Hunters: ${elk.hunters}</p>
+              <p>Days: ${elk.days}</p>
+              <p>Days per Hunter: ${elk.days_per_hunter}</p>
+              <p>Total Harvest: ${elk.total_harvest}</p>
+              <p>Bulls: ${elk.bulls}</p>
+              <p>Cows: ${elk.cows}</p>
+              <p>Calves: ${elk.calves}</p>
+              <p>Bow: ${elk.bow}</p>
+              <p>Rifle: ${elk.rifle}</p>
+              <p>Spiked Bull Elk: ${elk.spike_bull_elk}</p>
+              <p>Less than Six Points: ${elk.less_than_six_points}</p>
+              <p>Six or More Points: ${elk.six_or_more_points}</p>
+              <p>Location: ${elk.location}</p>
+              <p>District Square Mileage: ${elk.district_square_mileage}</p>
+              <p>Public Land Percentage: ${elk.public_land_percentage}</p>
+              <p>DIY Hunting Maps Success Rate: ${elk.diy_hunting_maps_hunt_success_rate}</p>
+              <p>Six or More Point Bulls Percentage: ${elk.six_or_more_point_bulls_percentage}%</p>
+              <p>Harvested Bulls per Square Mile: ${elk.harvested_bulls_per_square_mile}</p>
+              <p>Harvested Cows per Square Mile: ${elk.harvested_cows_per_square_mile}</p>
+              <p>Harvested Calves per Square Mile: ${elk.harvested_calves_per_square_mile}</p>
+              <p>Harvested Spiked Bulls per Square Mile: ${elk.harvested_spiked_bulls_per_square_mile}</p>
+              <p>Harvested Less than Six Point Bulls per Square Mile: ${elk.harvested_less_than_six_point_bulls_per_square_mile}</p>
+              <p>Harvested Six or More Point Bulls per Square Mile: ${elk.harvested_six_or_more_point_bulls_per_square_mile}</p>
+              <p>Total Harvest per Square Mile: ${elk.total_harvest_per_square_mile}</p>`;
             });
           } else {
             elkDataString = '<p>No data available for this district</p>';
@@ -117,7 +140,7 @@ const ElkAndDeerMap = () => {
     if (!map && mapContainer.current) {
       initializeMap({ setMap, mapContainer });
     }
-  }, [lat, lng, map, mapContainer, zoom, elkData]);
+  }, [lat, lng, map, mapContainer, zoom]);
   return (
     <div
       ref={mapContainer}
