d118f3a279d81ff7848459ca716144a5ba18d829
Kade Illian <kadeillianmt@gmail.com>
2023-02-23 23:29:07 -0700

    added a counties mapbox component that displays the elk b&c records

--- apps/smart-hunt/app/components/mapbox/CountiesMapbox.tsx
+++ apps/smart-hunt/app/components/mapbox/CountiesMapbox.tsx
@@ -3,10 +3,12 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import axios from "axios";
 
 mapboxgl.accessToken = "pk.eyJ1Ijoia2FkZWlsbGlhbjIxIiwiYSI6ImNsZG54MnZzZDBua2wzdXFwZHhxdzBva2gifQ.bANYko0jxjqxRWQaHSsq0g";
 
-const WolfMap = () => {
+
+const CountiesMap = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState(null);
   const [lng, setLng] = useState(-110.1);
@@ -22,6 +24,7 @@ const WolfMap = () => {
         zoom,
       });
 
+
       mapInstance.on("load", () => {
         setMap(mapInstance);
         mapInstance.resize();
@@ -32,25 +35,40 @@ const WolfMap = () => {
         });
 
         mapInstance.addLayer({
-          id: "county-layer",
+          id: "montana_counties",
+          type: "fill",
+          source: {
+            type: "vector",
+            url: "mapbox://kadeillian21.montana_counties"
+          },
+          "source-layer": "wolf",
+          paint: {
+            "fill-color": "orange",
+            "fill-opacity": 0.1,
+          },
+          minzoom: 0,
+          maxzoom: 22,
+        });
+        mapInstance.addLayer({
+          id: "montana_counties_line",
           type: "line",
           source: {
             type: "vector",
             url: "mapbox://kadeillian21.montana_counties"
           },
-          "source-layer": "wolf", // accidentally named wolf, is really counties
+          "source-layer": "wolf",
           paint: {
-            "line-color": "orange",
+            "line-color": "red",
             "line-width": 3,
           },
           minzoom: 0,
           maxzoom: 22,
         });
         mapInstance.addLayer({
-          id: "wolf-labels",
+          id: "county-labels",
           type: "symbol",
-          source: "wolf", // accidentally named wolf, is really counties
-          "source-layer": "wolf", // accidentally named wolf, is really counties
+          source: "wolf",
+          "source-layer": "wolf",
           layout: {
             "text-field": "{name}",
             "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
@@ -62,14 +80,35 @@ const WolfMap = () => {
           minzoom: 0,
           maxzoom: 22,
         });
+        mapInstance.on("click", "montana_counties", async (e) => {
+          const feature = e.features[0];
+          const county = feature.properties.name;
+          const response = await axios.get("http://localhost:4200/api/elk_boone_and_crockett");
+          const elkData = (response.data);
+          const countyElkData = elkData.data.filter(elk => elk.county === county);
+          let elkDataString = '';
+          if (countyElkData.length > 0) {
+            countyElkData.forEach(elk => {
+              elkDataString +=
+              `<p>Number of B&C Trophies Harvested: ${elk[" trophy_count"]}</p>
+              `
+            });
+          } else {
+            elkDataString = '<p>No data available for this district</p>';
+          }
+          new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(`<p>County: ${county}</p>${elkDataString}`)
+            .addTo(mapInstance);
+        });
       });
+
     };
 
     if (!map && mapContainer.current) {
       initializeMap({ setMap, mapContainer });
     }
   }, [lat, lng, map, mapContainer, zoom]);
-
   return (
     <div
       ref={mapContainer}
@@ -78,4 +117,4 @@ const WolfMap = () => {
   );
 };
 
-export default WolfMap;
+export default CountiesMap;
---
+++ apps/smart-hunt/pages/api/antelope_boone_and_crockett/index.ts
@@ -0,0 +1,37 @@
+import connectToDB from "../../../lib/connectToDB";
+
+export default async function handler(req, res) {
+  const client = await connectToDB();
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const result = await client.query("SELECT * FROM antelope_boone_and_crockett");
+        const antelope_boone_and_crocketts = result.rows;
+        res.status(200).json({ success: true, data: antelope_boone_and_crocketts });
+      } catch (error) {
+        console.error("Error querying antelope_boone_and_crockett table: ", error);
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const { name, weight, location } = req.body;
+        const result = await client.query(
+          "INSERT INTO antelope_boone_and_crockett (name, weight, location) VALUES ($1, $2, $3)",
+          [name, weight, location]
+        );
+        res.status(201).json({ success: true });
+      } catch (error) {
+        console.error("Error inserting into antelope_boone_and_crockett table: ", error);
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+
+  client.release();
+}
